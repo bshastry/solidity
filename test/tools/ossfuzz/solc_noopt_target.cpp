510bbbf8e5bbeee8bf0db7117d438d004d3c2b3d
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-01-16 12:10:41 +0100

    ossfuzz code share and refactor WIP

---
+++ test/tools/ossfuzz/solc_noopt_target.cpp
@@ -0,0 +1,8 @@
+#include "fuzzer.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    string input(reinterpret_cast<const char*>(data), size);
+    testCompiler(input, false);
+    return 0;
+}
---
+++ test/tools/ossfuzz/yulopti_target.cpp
@@ -0,0 +1,8 @@
+#include "yulopti.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    string input(reinterpret_cast<const char*>(data), size);
+    YulOpti{}.runNonInteractive(input);
+    return 0;
+}
