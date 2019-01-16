adc262ecbb1b6da59541cbc608f67971b1b4aa83
Bhargava Shastry <bshastry@ethereum.org>
2019-01-16 17:19:52 +0100

    ossfuzz afl only harness refactor wip

--- test/tools/ossfuzz/const_opt_target.cpp
+++ test/tools/ossfuzz/const_opt_target.cpp
@@ -3,6 +3,6 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     string input(reinterpret_cast<const char*>(data), size);
-    testConstantOptimizer(input);
+    testConstantOptimizer(input, true);
     return 0;
 }
