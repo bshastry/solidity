592959015425b12fc312b4a819c2004c83418f34
Bhargava Shastry <bshastry@ethereum.org>
2019-01-22 11:54:51 +0100

    Switched indent from spaces to tabs; renamed and moved utility function to where it better belongs

--- test/tools/fuzzer_common.cpp
+++ test/tools/fuzzer_common.cpp
@@ -16,106 +16,99 @@
 */
 
 #include <test/tools/fuzzer_common.h>
+#include <libdevcore/CommonData.h>
 
 using namespace std;
 using namespace dev;
 using namespace dev::eth;
 
-string contains(string const& _haystack, vector<string> const& _needles)
-{
-    for (string const& needle: _needles)
-        if (_haystack.find(needle) != string::npos)
-            return needle;
-    return "";
-}
-
 void FuzzerUtil::runCompiler(string _input)
 {
-    string outputString(solidity_compile(_input.c_str(), nullptr));
-    Json::Value output;
-    if (!jsonParseStrict(outputString, output))
-    {
-        cout << "Compiler produced invalid JSON output." << endl;
-        abort();
-    }
-    if (output.isMember("errors"))
-        for (auto const& error: output["errors"])
-        {
-            string invalid = contains(error["type"].asString(), vector<string>{
-                    "Exception",
-                    "InternalCompilerError"
-            });
-            if (!invalid.empty())
-            {
-                cout << "Invalid error: \"" << error["type"].asString() << "\"" << endl;
-                abort();
-            }
-        }
+	string outputString(solidity_compile(_input.c_str(), nullptr));
+	Json::Value output;
+	if (!jsonParseStrict(outputString, output))
+	{
+		cout << "Compiler produced invalid JSON output." << endl;
+		abort();
+	}
+	if (output.isMember("errors"))
+		for (auto const& error: output["errors"])
+		{
+			string invalid = findAnyOf(error["type"].asString(), vector<string>{
+					"Exception",
+					"InternalCompilerError"
+			});
+			if (!invalid.empty())
+			{
+				cout << "Invalid error: \"" << error["type"].asString() << "\"" << endl;
+				abort();
+			}
+		}
 }
 
 void FuzzerUtil::testCompiler(string const& _input, bool _optimize, bool _quiet)
 {
-    if (!_quiet)
-        cout << "Testing compiler " << (_optimize ? "with" : "without") << " optimizer." << endl;
+	if (!_quiet)
+		cout << "Testing compiler " << (_optimize ? "with" : "without") << " optimizer." << endl;
 
-    Json::Value config = Json::objectValue;
-    config["language"] = "Solidity";
-    config["sources"] = Json::objectValue;
-    config["sources"][""] = Json::objectValue;
-    config["sources"][""]["content"] = _input;
-    config["settings"] = Json::objectValue;
-    config["settings"]["optimizer"] = Json::objectValue;
-    config["settings"]["optimizer"]["enabled"] = _optimize;
-    config["settings"]["optimizer"]["runs"] = 200;
+	Json::Value config = Json::objectValue;
+	config["language"] = "Solidity";
+	config["sources"] = Json::objectValue;
+	config["sources"][""] = Json::objectValue;
+	config["sources"][""]["content"] = _input;
+	config["settings"] = Json::objectValue;
+	config["settings"]["optimizer"] = Json::objectValue;
+	config["settings"]["optimizer"]["enabled"] = _optimize;
+	config["settings"]["optimizer"]["runs"] = 200;
 
-    // Enable all SourceUnit-level outputs.
-    config["settings"]["outputSelection"]["*"][""][0] = "*";
-    // Enable all Contract-level outputs.
-    config["settings"]["outputSelection"]["*"]["*"][0] = "*";
+	// Enable all SourceUnit-level outputs.
+	config["settings"]["outputSelection"]["*"][""][0] = "*";
+	// Enable all Contract-level outputs.
+	config["settings"]["outputSelection"]["*"]["*"][0] = "*";
 
-    runCompiler(jsonCompactPrint(config));
+	runCompiler(jsonCompactPrint(config));
 }
 
 void FuzzerUtil::testConstantOptimizer(string const& _input, bool _quiet)
 {
-    if (!_quiet)
-        cout << "Testing constant optimizer" << endl;
-    vector<u256> numbers;
-    stringstream sin(_input);
+	if (!_quiet)
+		cout << "Testing constant optimizer" << endl;
+	vector<u256> numbers;
+	stringstream sin(_input);
 
-    while (!sin.eof())
-    {
-        h256 data;
-        sin.read(reinterpret_cast<char *>(data.data()), 32);
-        numbers.push_back(u256(data));
-    }
-    if (!_quiet)
-        cout << "Got " << numbers.size() << " inputs:" << endl;
+	while (!sin.eof())
+	{
+		h256 data;
+		sin.read(reinterpret_cast<char *>(data.data()), 32);
+		numbers.push_back(u256(data));
+	}
+	if (!_quiet)
+		cout << "Got " << numbers.size() << " inputs:" << endl;
 
-    Assembly assembly;
-    for (u256 const &n: numbers)
-    {
-        if (!_quiet)
-            cout << n << endl;
-        assembly.append(n);
-    }
-    for (bool isCreation: {false, true})
-        for (unsigned runs: {1, 2, 3, 20, 40, 100, 200, 400, 1000})
-        {
-            ConstantOptimisationMethod::optimiseConstants(
-                    isCreation,
-                    runs,
-                    EVMVersion{},
-                    assembly,
-                    const_cast<AssemblyItems &>(assembly.items())
-            );
-        }
+	Assembly assembly;
+	for (u256 const& n: numbers)
+	{
+		if (!_quiet)
+			cout << n << endl;
+		assembly.append(n);
+	}
+	for (bool isCreation: {false, true})
+		for (unsigned runs: {1, 2, 3, 20, 40, 100, 200, 400, 1000})
+		{
+			ConstantOptimisationMethod::optimiseConstants(
+					isCreation,
+					runs,
+					EVMVersion{},
+					assembly,
+					const_cast<AssemblyItems &>(assembly.items())
+			);
+		}
 }
 
 void FuzzerUtil::testStandardCompiler(string const& _input, bool _quiet)
 {
-    if (!_quiet)
-        cout << "Testing compiler via JSON interface." << endl;
+	if (!_quiet)
+		cout << "Testing compiler via JSON interface." << endl;
 
-    runCompiler(_input);
+	runCompiler(_input);
 }
--- test/tools/ossfuzz/solc_opt_ossfuzz.cpp
+++ test/tools/ossfuzz/solc_opt_ossfuzz.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
 {
-    string input(reinterpret_cast<char const*>(_data), _size);
-    FuzzerUtil::testCompiler(input, /*optimize=*/true, /*quiet=*/true);
-    return 0;
+	string input(reinterpret_cast<char const*>(_data), _size);
+	FuzzerUtil::testCompiler(input, /*optimize=*/true, /*quiet=*/true);
+	return 0;
 }
