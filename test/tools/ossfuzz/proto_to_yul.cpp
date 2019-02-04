deff99f8182f93af75d6d262adc691e3548af7d4
Bhargava Shastry <bshastry@ethereum.org>
2019-02-04 13:19:52 +0100

    Code style changes and added vardecl wip

--- test/tools/ossfuzz/proto_to_yul.cpp
+++ test/tools/ossfuzz/proto_to_yul.cpp
@@ -24,89 +24,192 @@
 namespace yul_fuzzer {
 
 // Forward decls.
-std::ostream &operator<<(std::ostream &os, const BinaryOp &x);
-std::ostream &operator<<(std::ostream &os, const StatementSeq &x);
+	std::ostream &operator<<(std::ostream &os, const BinaryOp &x);
 
-// Proto to C++.
-std::ostream &operator<<(std::ostream &os, const Const &x) {
-  return os << "(" << x.val() << ")";
-}
-std::ostream &operator<<(std::ostream &os, const VarRef &x) {
-  return os << "a[" << (static_cast<uint32_t>(x.varnum()) % 100) << "]";
-}
-std::ostream &operator<<(std::ostream &os, const Lvalue &x) {
-  return os << x.varref();
-}
-std::ostream &operator<<(std::ostream &os, const Rvalue &x) {
-    if (x.has_varref()) return os << x.varref();
-    if (x.has_cons())   return os << x.cons();
-    if (x.has_binop())  return os << x.binop();
-    return os << "1";
-}
-std::ostream &operator<<(std::ostream &os, const BinaryOp &x) {
-  switch (x.op()) {
-    case BinaryOp::PLUS: os << "add(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::MINUS: os << "sub(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::MUL: os << "mul(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::DIV: os << "div(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::MOD: os << "mod(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::XOR: os << "xor(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::AND: os << "and(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::OR: os << "or(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::EQ: os << "eq(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::LT: os << "lt(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::GT: os << "gt(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::MSTORE: os << "mstore(" << x.left() << "," << x.right() << ")"; break;
-    case BinaryOp::SSTORE: os << "sstore(" << x.left() << "," << x.right() << ")"; break;
-  }
-  return os;
-}
-std::ostream &operator<<(std::ostream &os, const UnaryOp &x) {
-  switch (x.op()) {
-    case UnaryOp::NOT: os << "not(" << x.operand() << ")"; break;
-    case UnaryOp::MLOAD: os << "mload(" << x.operand() << ")"; break;
-    case UnaryOp::SLOAD: os << "sload(" << x.operand() << ")"; break;
-  }
-  return os;
-}
-std::ostream &operator<<(std::ostream &os, const AssignmentStatement &x) {
-  return os << x.lvalue() << "=" << x.rvalue() << ";\n";
-}
-std::ostream &operator<<(std::ostream &os, const IfElse &x) {
-  return os << "if (" << x.cond() << "){\n"
-            << x.if_body() << "} else { \n"
-            << x.else_body() << "}\n";
-}
-std::ostream &operator<<(std::ostream &os, const While &x) {
-  return os << "while (" << x.cond() << "){\n" << x.body() << "}\n";
-}
-std::ostream &operator<<(std::ostream &os, const Statement &x) {
-  if (x.has_assignment()) return os << x.assignment();
-  if (x.has_ifelse())     return os << x.ifelse();
-  if (x.has_while_loop()) return os << x.while_loop();
-  return os << "(void)0;\n";
-}
-std::ostream &operator<<(std::ostream &os, const StatementSeq &x) {
-  for (auto &st : x.statements()) os << st;
-  return os;
-}
-std::ostream &operator<<(std::ostream &os, const Function &x) {
-  return os << "void foo(int *a) {\n" << x.statements() << "}\n";
-}
+	std::ostream &operator<<(std::ostream &os, const StatementSeq &x);
+
+	std::ostream &operator<<(std::ostream &os, const VarDecl &x)
+	{
+		os << "let x_" << x.id() << " := " << x.expr() << ":";
+		switch (x.type())
+		{
+			case VarDecl::BOOL:
+				os << "bool\n";
+				break;
+			case VarDecl::S8:
+				os << "s8\n";
+				break;
+			case VarDecl::S32:
+				os << "s32\n";
+				break;
+			case VarDecl::S64:
+				os << "s64\n";
+				break;
+			case VarDecl::S128:
+				os << "s128\n";
+				break;
+			case VarDecl::S256:
+				os << "s256\n";
+				break;
+			case VarDecl::U8:
+				os << "u8\n";
+				break;
+			case VarDecl::U32:
+				os << "u32\n";
+				break;
+			case VarDecl::U64:
+				os << "u64\n";
+				break;
+			case VarDecl::U128:
+				os << "u128\n";
+				break;
+			case VarDecl::U256:
+				os << "u256\n";
+				break;
+		}
+		return os;
+	}
+
+	// Proto to yul.
+	// Constant is a signed 32-bit integer
+	std::ostream &operator<<(std::ostream &os, const Const &x)
+	{
+		return os << "(" << x.val() << ")";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const VarRef &x)
+	{
+		return os << "a[" << (static_cast<uint32_t>(x.varnum()) % 100) << "]";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const Lvalue &x)
+	{
+		return os << x.varref();
+	}
+
+	std::ostream &operator<<(std::ostream &os, const Rvalue &x)
+	{
+		if (x.has_varref())
+			return os << x.varref();
+		if (x.has_cons())
+			return os << x.cons();
+		if (x.has_binop())
+			return os << x.binop();
+		return os << "1";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const BinaryOp &x)
+	{
+		switch (x.op()) {
+			case BinaryOp::PLUS:
+				os << "add(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::MINUS:
+				os << "sub(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::MUL:
+				os << "mul(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::DIV:
+				os << "div(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::MOD:
+				os << "mod(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::XOR:
+				os << "xor(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::AND:
+				os << "and(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::OR:
+				os << "or(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::EQ:
+				os << "eq(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::LT:
+				os << "lt(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::GT:
+				os << "gt(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::MSTORE:
+				os << "mstore(" << x.left() << "," << x.right() << ")";
+				break;
+			case BinaryOp::SSTORE:
+				os << "sstore(" << x.left() << "," << x.right() << ")";
+				break;
+		}
+		return os;
+	}
+
+	std::ostream &operator<<(std::ostream &os, const UnaryOp &x)
+	{
+		switch (x.op()) {
+			case UnaryOp::NOT:
+				os << "not(" << x.operand() << ")";
+				break;
+			case UnaryOp::MLOAD:
+				os << "mload(" << x.operand() << ")";
+				break;
+			case UnaryOp::SLOAD:
+				os << "sload(" << x.operand() << ")";
+				break;
+		}
+		return os;
+	}
+
+	std::ostream &operator<<(std::ostream &os, const AssignmentStatement &x)
+	{
+		return os << x.lvalue() << " := " << x.rvalue() << "\n";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const IfElse &x)
+	{
+		return os << "if (" << x.cond() << "){\n"
+		          << x.if_body() << "} else { \n"
+		          << x.else_body() << "}\n";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const While &x)
+	{
+		return os << "while (" << x.cond() << "){\n" << x.body() << "}\n";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const Statement &x)
+	{
+		if (x.has_assignment()) return os << x.assignment();
+		if (x.has_ifelse()) return os << x.ifelse();
+		if (x.has_while_loop()) return os << x.while_loop();
+		return os << "\n";
+	}
+
+	std::ostream &operator<<(std::ostream &os, const StatementSeq &x)
+	{
+		for (auto &st : x.statements()) os << st;
+		return os;
+	}
+//std::ostream &operator<<(std::ostream &os, const Function &x) {
+//  return os << "void foo(int *a) {\n" << x.statements() << "}\n";
+//}
 
 // ---------------------------------
 
-std::string FunctionToString(const Function &input) {
-  std::ostringstream os;
-  os << input;
-  return os.str();
+	std::string FunctionToString(const Function &input)
+	{
+		std::ostringstream os;
+		os << input;
+		return os.str();
 
-}
-std::string ProtoToYul(const uint8_t *data, size_t size) {
-  Function message;
-  if (!message.ParsePartialFromArray(data, size))
-    return "#error invalid proto\n";
-  return FunctionToString(message);
-}
+	}
+
+	std::string ProtoToYul(const uint8_t *data, size_t size)
+	{
+		Function message;
+		if (!message.ParsePartialFromArray(data, size))
+			return "#error invalid proto\n";
+		return FunctionToString(message);
+	}
 
 } // namespace yul_fuzzer
