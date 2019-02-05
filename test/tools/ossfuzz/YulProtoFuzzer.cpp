10854913d24a730b319ffe60907a88f4f58a8b2e
Bhargava Shastry <bshastry@ethereum.org>
2019-02-05 15:58:18 +0100

    Minor bug fix

--- test/tools/ossfuzz/YulProtoFuzzer.cpp
+++ test/tools/ossfuzz/YulProtoFuzzer.cpp
@@ -15,6 +15,8 @@
 	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <fstream>
+
 #include <test/tools/ossfuzz/yul_proto.pb.h>
 #include <test/tools/fuzzer_common.h>
 #include <test/tools/ossfuzz/proto_to_yul.h>
